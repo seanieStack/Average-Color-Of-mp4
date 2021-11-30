306a53e3e8456e296ff5c37c9aba63f8e4af5685
Seanie Stack <65853902+seanieStack@users.noreply.github.com>
2021-11-30 12:10:34 +0000

    Completely Reworked Logic
    This was reworked in May for A CS50x Project, but I lost commit history. Program runs much quicker. Using Numpy array functions greatly improved functionality.

--- averageColor.py
+++ averageColor.py
@@ -1,63 +1,167 @@
 import cv2
 import numpy as np
 import os
+import sys
+import shutil
 from PIL import Image
+import easygui
 
-cap = cv2.VideoCapture('sherk.mp4')
+INPUT_FPS = 1
 
+# checks if ./temp exisits, if yes, it deletes it and all its content, then creates it, if no it creates it
 try:
-    if not os.path.exists('data'):
-        os.makedirs('data')
+    if os.path.exists('./temp'):
+        shutil.rmtree("./temp")
+
+    os.makedirs('./temp')
 except OSError:
     print("Error: Creating Derictory of data")
 
-currentFrame = 0
-length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-while (currentFrame < length):
+inputVideo = easygui.fileopenbox(filetypes=["*.mp4"])
+
+if inputVideo == None:
+    sys.exit("Please Select A video File")
+
+# sets up our capture object
+cap = cv2.VideoCapture(inputVideo)
+
+if not cap.isOpened():
+    sys.exit("Error Creating Capture Device")
+
+# gets the number of frames for the input video
+length_of_input_video = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+INPUT_FPS = cap.get(cv2.CAP_PROP_FPS)
+
+
+# for counter
+lastPercent = 0
+
+# loops through all frames in video
+for currentFrame in range(0, length_of_input_video):
+
+    # reads current frame
     ret, frame = cap.read()
 
-    name = './data/frame' + str(currentFrame) + '.jpg'
-    print('Creating...' + name)
+    # if current frame doesn't exist
+    if ret == False:
+        break
+
+    currentPercent = round((currentFrame / length_of_input_video)*100)
+    if currentPercent != lastPercent:
+        print(
+            f"Creating Frames: {round((currentFrame /  length_of_input_video)*100)}%")
+        lastPercent = currentPercent
+
+    # sets the name of the next frame
+    name = f'./temp/frame{str(currentFrame)}.jpg'
+
+    # saves an image of the currrent frame
     cv2.imwrite(name, frame)
 
-    currentFrame += 1
-
+# closes video capture objects
 cap.release()
-cv2.destroyAllWindows()    
+cv2.destroyAllWindows()
 
-def getAverageColor(image_path):
+# creates arrary to list of arrays
+averageColor = []
 
-    image = Image.open(curImage)
-    width, height = 0, 0
-    width, height = image.size
+# function to add the average color to array
 
-    r, g, b = 0, 0, 0
-    count = 0
 
-    for s in range(1, width):
-        for t in range(1, height):
-            pixl_r, pixl_g, pixl_b = image.getpixel((s, t))
-            r += pixl_r
-            b += pixl_b
-            g += pixl_g
-            count += 1
-    return ((r//count), (g//count), (b//count))
+def colorOfCurrentFrame(frame):
 
-DIR = './data'
-files = next(os.walk(DIR))[2]
-numOfFrames = len(files)
-
-for q in range(1, numOfFrames):
     try:
-        if not os.path.exists('fin'):
-            os.makedirs('fin')
-    except OSError:
-        print("Error: Creating Derictory of data")
+        # creates a img object
+        img = cv2.imread(frame, cv2.IMREAD_COLOR)
 
-    curImage = DIR + "/frame" + str(q) + ".jpg"
-    color = getAverageColor(curImage)
+        # gets height and width of the current image
+        height, width, channels = img.shape
 
-    print(color)
+        # gets the average of the 3d array
+        bgr = np.average(np.average(img, axis=0), axis=0)
 
-    newImg = Image.new('RGB', (1920, 1080), color)
-    newImg = newImg.save("./fin/newFrame" + str(q) + ".jpg")
+        # adds new color to end of color array
+        averageColor.append(bgr)
+    except:
+        sys.exit("Error Reading Frame")
+
+
+# checks if ./tmp exisits, if yes, it deletes it and all its content, then creates it, if no it creates it
+try:
+    if os.path.exists('./tmp'):
+        shutil.rmtree("./tmp")
+
+    os.makedirs('./tmp')
+except OSError:
+    print("Error: Creating Derictory of data")
+
+# loops through frames
+averageColorPercent = 0
+for i in range(0, len(next(os.walk("./temp"))[2])):
+    currentPercent = round(i / len(next(os.walk("./temp"))[2]) * 100)
+
+    if currentPercent != averageColorPercent:
+        print(f"Getting Average Color: {currentPercent}%")
+        averageColorPercent = currentPercent
+
+    colorOfCurrentFrame(f"./temp/frame{i}.jpg")
+
+i = 0
+framePercent = 0
+for color in averageColor:
+    # percent counter
+    currentPercent = round((i / len(averageColor)) * 100)
+    if currentPercent != framePercent:
+        print(f"Generating New Frames: {currentPercent}%")
+        framePercent = currentPercent
+
+    # creates a new image with each color
+    img = Image.new('RGB', (1920, 1080), (int(
+        color[2]), int(color[1]), int(color[0])))
+    ret = img.save(f"./tmp/finished{i}.jpg")
+
+    i = i + 1
+
+images = []
+# loops through all images in tmp dir
+tmpPercent = 0
+for i in range(0, len(next(os.walk("./tmp"))[2])):
+
+    # percent counter
+    currentPercent = round((i / len(next(os.walk('./tmp'))[2])) * 100)
+    if currentPercent != tmpPercent:
+        print(
+            f"Gathering Images { currentPercent}%")
+        tmpPercent = currentPercent
+
+    # addes image location to end of image array
+    images.append(f"./tmp/finished{i}.jpg")
+
+
+# setup for video writer
+frame = cv2.imread(images[0])
+height, width, layers = frame.shape
+video = cv2.VideoWriter("output.avi", 0, INPUT_FPS, (width, height))
+
+it = 0
+imgPercent = 0
+
+# loops though all images in images array
+for image in images:
+    # percent counter
+    currentPercent = round((it / len(images)) * 100)
+    if currentPercent != imgPercent:
+        print(f"Writing Images to Video {currentPercent}%")
+        imgPercent = currentPercent
+    it += 1
+
+    # writes each image to the end of the video
+    video.write(cv2.imread(image))
+
+# closes all windows and releases video object
+cv2.destroyAllWindows()
+video.release()
+
+# deletes all tmp dirs
+shutil.rmtree("./temp")
+shutil.rmtree("./tmp")
